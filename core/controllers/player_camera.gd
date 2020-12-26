6c54a28b3003c4c31370cc797c54bc7c5bcca079
Shatur95 <genaloner@gmail.com>
2020-12-26 16:37:23 +0200

    Add axis camera rotation

--- core/controllers/player_camera.gd
+++ core/controllers/player_camera.gd
@@ -4,7 +4,9 @@ extends SpringArm
 # For this reason, cameras only have relevance to human-controlled players.
 
 
-const ROTATION_SPEED := 0.002
+const MOUSE_ROTATION_SPEED := 0.002
+const AXIS_ROTATION_SPEED := 0.03
+const AXIS_DEADZONE := 0.03
 const ZOOM_SPEED := 0.5
 
 onready var _camera: Camera = $Camera
@@ -12,6 +14,17 @@ onready var _camera: Camera = $Camera
 func _ready() -> void:
 	if OS.is_window_focused():
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+
+func _process(_delta: float) -> void:
+	var vertical_axis: float = Input.get_joy_axis(0, JOY_AXIS_2)
+	if abs(vertical_axis) > AXIS_DEADZONE:
+		rotation.y -= vertical_axis * AXIS_ROTATION_SPEED
+
+	var horizontal_axis: float = Input.get_joy_axis(0, JOY_AXIS_3)
+	if abs(vertical_axis) > AXIS_DEADZONE:
+		rotation.x -= horizontal_axis * AXIS_ROTATION_SPEED
+		rotation.x = clamp(rotation.x, -PI / 2, 0.7)
 
 
 func _input(event: InputEvent) -> void:
@@ -25,6 +38,6 @@ func _input(event: InputEvent) -> void:
 	elif event.is_action_pressed("zoom_out"):
 		spring_length -= ZOOM_SPEED
 	elif Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED and event is InputEventMouseMotion:
-		rotation.y -= event.relative.x * ROTATION_SPEED
-		rotation.x -= event.relative.y * ROTATION_SPEED
+		rotation.y -= event.relative.x * MOUSE_ROTATION_SPEED
+		rotation.x -= event.relative.y * MOUSE_ROTATION_SPEED
 		rotation.x = clamp(rotation.x, -PI / 2, 0.7)
