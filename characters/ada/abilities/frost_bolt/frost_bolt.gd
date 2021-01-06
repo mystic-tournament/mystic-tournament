edeaad51a19ebea222ffc28e57ac29ba5e24f84e
Hennadii Chernyshchyk <genaloner@gmail.com>
2021-01-07 00:37:40 +0200

    Add FrostBolt projectile model

--- characters/ada/abilities/frost_bolt/frost_bolt.gd
+++ characters/ada/abilities/frost_bolt/frost_bolt.gd
@@ -3,7 +3,7 @@ extends BaseAbility
 
 
 func use(caster: BaseHero, transform: Transform) -> void:
-	var frost_bold: KinematicBody = preload("res://characters/ada/abilities/frost_bolt_projectile.tscn").instance()
+	var frost_bold: KinematicBody = preload("res://characters/ada/abilities/frost_bolt/frost_bolt_projectile.tscn").instance()
 	frost_bold.global_transform = transform
 	frost_bold.caster = caster
 	GameSession.map.add_child(frost_bold)
