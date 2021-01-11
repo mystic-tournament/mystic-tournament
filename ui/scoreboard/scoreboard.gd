2b62296cbb56ce5dc7fd7cd9d5e8d7bce7eaa09c
Shatur95 <genaloner@gmail.com>
2021-01-11 22:52:08 +0200

    Use holding button to display scoreboard

--- ui/scoreboard/scoreboard.gd
+++ ui/scoreboard/scoreboard.gd
@@ -6,11 +6,13 @@ onready var _tree: ScoreboardTree = $Tree
 
 
 func _input(event: InputEvent) -> void:
-	if event.is_pressed() and event.is_action("ui_scoreboard"):
-		if visible:
-			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-			hide()
-		else:
-			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-			_tree.update_info()
-			show()
+	if not event.is_action("ui_scoreboard"):
+		return
+
+	if visible and not event.is_pressed():
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		hide()
+	elif not visible and event.is_pressed():
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		_tree.update_info()
+		show()
