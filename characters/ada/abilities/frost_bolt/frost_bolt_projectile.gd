6b396b5f8adce0afe826d2aef1f2deb314e3518e
Shatur95 <genaloner@gmail.com>
2021-01-14 23:56:56 +0200

    Remove extra normalization

--- characters/ada/abilities/frost_bolt/frost_bolt_projectile.gd
+++ characters/ada/abilities/frost_bolt/frost_bolt_projectile.gd
@@ -7,7 +7,7 @@ var caster: BaseHero
 
 
 func _physics_process(delta: float) -> void:
-	var collision: KinematicCollision = move_and_collide(-global_transform.basis.z.normalized() * delta * SPEED)
+	var collision: KinematicCollision = move_and_collide(-global_transform.basis.z * delta * SPEED)
 	if not collision:
 		return
 
