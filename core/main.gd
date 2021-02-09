14f90aa574bf6f5e675f94f2dcf9bb3c2760d2c8
Shatur95 <genaloner@gmail.com>
2021-02-09 18:39:17 +0200

    Use single function for game end

--- core/main.gd
+++ core/main.gd
@@ -27,7 +27,7 @@ func _on_session_started() -> void:
 	add_child(GameSession.map)
 
 	# warning-ignore:return_value_discarded
-	GameSession.gamemode.connect("game_over", self, "_on_game_over")
+	GameSession.gamemode.connect("game_over", self, "_end_game")
 
 	if CmdArguments.server:
 		return
@@ -38,7 +38,7 @@ func _on_session_started() -> void:
 	_ingame_menu = IngameMenuScene.instance()
 	add_child(_ingame_menu)
 	# warning-ignore:return_value_discarded
-	_ingame_menu.connect("leave_pressed", self, "_leave_game")
+	_ingame_menu.connect("leave_pressed", self, "_end_game")
 
 	_scoreboard = ScoreboardScene.instance()
 	_ui.add_child(_scoreboard)
@@ -46,12 +46,13 @@ func _on_session_started() -> void:
 	_chat.move_upper()
 
 
-# TODO 4.0: Unbind extra _winner argument
-func _on_game_over(_winner) -> void:
-	_fade_rect = FadeRectScene.instance()
-	add_child(_fade_rect)
-	_fade_rect.fade_out()
-	yield(_fade_rect, "finished")
+# Show animation and scoreboard on normal termination (winner != null), otherwise just close the game session
+func _end_game(winner = null) -> void:
+	if winner:
+		_fade_rect = FadeRectScene.instance()
+		add_child(_fade_rect)
+		_fade_rect.fade_out()
+		yield(_fade_rect, "finished")
 
 	if CmdArguments.server:
 		get_tree().quit()
@@ -59,25 +60,15 @@ func _on_game_over(_winner) -> void:
 	GameSession.clear()
 	_hud.queue_free()
 	_ingame_menu.queue_free()
-	_scoreboard.show_final_score()
 	_chat.move_down()
-	_fade_rect.fade_in()
-	yield(_fade_rect, "finished")
 
-	_fade_rect.queue_free()
-	yield(_scoreboard, "closed")
+	if winner:
+		_scoreboard.show_final_score()
+		_fade_rect.fade_in()
+		yield(_fade_rect, "finished")
+		_fade_rect.queue_free()
+		yield(_scoreboard, "closed")
 
 	_scoreboard.queue_free()
-	_main_menu = MainMenuScene.instance()
-	_ui.add_child(_main_menu)
-
-
-func _leave_game() -> void:
-	get_tree().network_peer = null
-	GameSession.clear()
-	_hud.queue_free()
-	_ingame_menu.queue_free()
-	_scoreboard.queue_free()
-	_chat.move_down()
 	_main_menu = MainMenuScene.instance()
 	_ui.add_child(_main_menu)
